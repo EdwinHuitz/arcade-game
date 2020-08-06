160ee58216186b34c4aa93120da7490fb25581a3
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-08-06 09:53:08 -0400

    adding the score and a reset button
    Adding the score system and a working reset button

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ const Lvl1 = [
 ];
 //declaring objects
 const gBoard = document.getElementById("board");
+//declaring the current score and highscore
+const curScore=document.getElementById("curScore");
+const highScore='';
 //variables
 //current score and high score names
 let cScore=0;
@@ -35,6 +38,7 @@ let pY=10;
 //event listeners
 //button clicks
 document.getElementById("startgame").addEventListener('click',init);
+document.getElementById("reStart").addEventListener('click',reStart);
 document.getElementById("mUp").addEventListener('click',moveUp);
 document.getElementById("mDown").addEventListener('click',moveDown);
 document.getElementById("mLeft").addEventListener('click',moveLeft);
@@ -46,6 +50,7 @@ window.addEventListener('keydown',a=>movePlayer(a));
 //sets up the gameboard and initializes the game
 function init()
 {
+    curScore.innerHTML=cScore;
     gBoard.innerHTML="";
     document.getElementById("btnBar").style.visibility="visible";
     //building lvl 1
@@ -94,39 +99,65 @@ function movePlayer(a)
 //Player movement
 function moveUp()
 {
+    //gets the player's current position
     let P1 = document.getElementById(`c${pY}r${pX}`);
+    //gets the player's next position
     let P2 = document.getElementById(`c${pY-1}r${pX}`);
+    //checks if the player can move to this spot
     if(pY>0 && P2.className=="Path"){
-    P1.innerHTML="";
-    P2.innerHTML=playr;
-    pY--;}
+        //checks for powerpoints
+        if(P2.innerHTML==pP)
+        {
+            cScore+=10;
+            curScore.innerHTML=cScore;
+        }
+        //moves the player
+        P1.innerHTML="";
+        P2.innerHTML=playr;
+        //updates the player's position
+        pY--;}
 }
 function moveDown()
 {
     let P1 = document.getElementById(`c${pY}r${pX}`);
     let P2 = document.getElementById(`c${pY+1}r${pX}`);
     if(pY<20 && P2.className=="Path"){
-    P1.innerHTML="";
-    P2.innerHTML=playr;
-    pY++;}
+        if(P2.innerHTML==pP)
+        {
+            cScore+=10;
+            curScore.innerHTML=cScore;
+        }
+        P1.innerHTML="";
+        P2.innerHTML=playr;
+        pY++;}
 }
 function moveLeft()
 {
     let P1 = document.getElementById(`c${pY}r${pX}`);
     let P2 = document.getElementById(`c${pY}r${pX-1}`);
     if(pX>0 && P2.className=="Path"){
-    P1.innerHTML="";
-    P2.innerHTML=playr;
-    pX--;}
+        if(P2.innerHTML==pP)
+        {
+            cScore+=10;
+            curScore.innerHTML=cScore;
+        }
+        P1.innerHTML="";
+        P2.innerHTML=playr;
+        pX--;}
 }
 function moveRight()
 {
     let P1 = document.getElementById(`c${pY}r${pX}`);
     let P2 = document.getElementById(`c${pY}r${pX+1}`);
     if(pX<20 && P2.className=="Path"){
-    P1.innerHTML="";
-    P2.innerHTML=playr;
-    pX++;}
+        if(P2.innerHTML==pP)
+        {
+            cScore+=10;
+            curScore.innerHTML=cScore;
+        }
+        P1.innerHTML="";
+        P2.innerHTML=playr;
+        pX++;}
 }
 console.log(Lvl1[10][10]);
 function addPP()
@@ -137,25 +168,25 @@ function addPP()
         a=Math.floor(Math.random() * Math.floor(21));
         b=Math.floor(Math.random() * Math.floor(21));
         c=Lvl1[a][b];
-        if(c==0 && c!=2)
-        {
+        if(c==0 && c!=2){
             document.getElementById(`c${a}r${b}`).innerHTML=pP;
             i++;
         }
-    }while(i<10);
+    }while(i<20);
 }
-/*point pellet logic: 
-
-
-
-*/
-
-// make another set of arrays which copy the first. This will be used as a guide by the point balls to help randomize their starting positions
-// make all the other essential variables like high-score, lifes, and x,y grid-positions for the player and enemies
-// design the point balls to generate on random 0's and detect whether the user has come in contact with them
-// design the score system and set it to increase by 10 for each point ball that is picked up by the player. every 100 points will grant the player another life
+//resets all variables and restarts the game
+function reStart()
+{
+    gBoard.innerHTML="";
+    cScore=0;
+    pX=10;pY=10;
+    iX=0;iY=0;
+    init();
+}
+// x,y grid-positions for the enemies
+// every 100 points will grant the player another life
 // let the enemies travel around the grid using only 0s by checking their surrounding every time they move and choose their movement direction based on that and a random number
 // check during every move if the enemies have collided with the player and if they have, remove 1 life from the player and set the player to lose should their remaining lives reach negative
 // setup the winning and losing/dying animations
 //add animations for the characters and point balls
-// set up a reset function to reset everything once the player runs out of lives or wants to start over
+// restart once the player runs out of lives
